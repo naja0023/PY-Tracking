f13444cee20afaa198d9925b8b816020cfe6cc2b
suphakorn <naja.67130@gmail.com>
2021-07-15 03:51:01 +0700

    .

--- Mobile/lib/secrets.dart
+++ Mobile/lib/secrets.dart
@@ -1,4 +1,4 @@
 class Secrets {
   // Add your Google Maps API Key here
-  static const API_KEY = 'AIzaSyCL8nqsRU9BK4t9TRSbXVpBwQy-7YfOSP0';
+  static const API_KEY = 'AIzaSyBSaaROLQQlFY4xMlTf9plC925bsdnbDGg';
 }
