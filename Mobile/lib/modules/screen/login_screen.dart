51cb540b1ef6f2695000145d791a6d9f3111b126
suphakorn <naja.67130@gmail.com>
2021-07-21 02:44:39 +0700

    ระบบล็อกอิน

--- Mobile/lib/modules/screen/login_screen.dart
+++ Mobile/lib/modules/screen/login_screen.dart
@@ -11,6 +11,7 @@ import 'package:fluttermqttnew/modules/helpers/status_info_message_utils.dart';
 import 'package:fluttermqttnew/modules/widgets/show_title.dart';
 import 'package:fluttermqttnew/utillity/my_constant.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class login extends StatefulWidget {
   const login({Key? key}) : super(key: key);
@@ -23,6 +24,9 @@ class _loginState extends State<login> {
   late Timer _timer;
   late MQTTManager _manager;
   var _counter = 0;
+  var _url = Uri.parse('http://10.0.2.2:35000/login');
+  final TextEditingController _getUsername = TextEditingController();
+  final TextEditingController _getPassword = TextEditingController();
   void initState() {
     // TODO: implement initState
     super.initState();
@@ -86,6 +90,7 @@ class _loginState extends State<login> {
           margin: EdgeInsets.only(top: 25),
           width: size * 0.6,
           child: TextFormField(
+            controller: _getUsername,
             decoration: InputDecoration(
               labelText: 'Enter Username',
               prefixIcon: Icon(Icons.account_circle),
@@ -114,6 +119,7 @@ class _loginState extends State<login> {
           margin: EdgeInsets.only(top: 15),
           width: size * 0.6,
           child: TextFormField(
+            controller: _getPassword,
             obscureText: statusRedEye,
             decoration: InputDecoration(
               suffixIcon: IconButton(
@@ -162,7 +168,7 @@ class _loginState extends State<login> {
             child: Text("Login"),
             style: MyConstant().MyButtonStlye(),
             onPressed: () {
-              _configureAndConnect();
+              _login();
             },
           ),
         ),
@@ -211,5 +217,20 @@ class _loginState extends State<login> {
   void _subscribe() {
     _manager.subScribeTo("moyanyo");
     Navigator.of(context).pushNamed(SETTINGS_ROUTE);
+  }
+
+  _login() async {
+    var username = _getUsername.text;
+    var password = _getPassword.text;
+    try {
+      http.Response response = await http.post(_url, body: {
+        'username': username,
+        'password': password,
+      }).timeout(Duration(seconds: 4));
+      var _check = response.body;
+      if (response.statusCode == 200) {
+        _configureAndConnect();
+      }
+    } catch (e) {}
   }
 }
