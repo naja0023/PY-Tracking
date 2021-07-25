b840602e5d898b507a2103b2eef7b9e00bc3a265
mukuro557 <6131302005@lamduan.mfu.ac.th>
2021-07-25 11:12:40 +0700

    Revert "Merge branch 'fix_something' into WEB-V1"
    This reverts commit 9b0237f35fccc40f40f3d2d5f65fd4eaaaa9da13, reversing
    changes made to 275d61d00a20363bcb93d33abde02783ec5baa6f.

---
+++ web/example-app/storage/framework/views/aa6186ac17e733f77b7d436dd319b3c3f368b9c6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>หน้าแรก</title>
+</head>
+<body>
+    <h1>หน้าแรก</h1>
+    <a href="<?php echo e(url('/')); ?>">Home</a>
+    <a href="<?php echo e(url('userlocate')); ?>">Users</a>
+    <a href="<?php echo e(url('driverlocate')); ?>">Driver</a>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\61020908\example-app\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ web/example-app/storage/framework/views/d9681b6300288a8930f951e593c1bee6af5b57a6.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body>
+    <h1>สวัสดีแอดมิน</h1>
+    <a href="/adad"></a>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\Test\example-app\resources\views/admin/welcome.blade.php ENDPATH**/ ?>
