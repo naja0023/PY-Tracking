1cb5c9941b073388422a5c504cadc155597f8bb2
hondahwn <79396857+hondahwn@users.noreply.github.com>
2021-08-15 02:00:21 +0700

    จัดหน้า

--- web/public/scripts/js/index.js
+++ web/public/scripts/js/index.js
@@ -1,4 +1,4 @@
-var lat 
+var lat
 var lng
 var beachMarker
 var connection = new WebSocket('ws://localhost:34000')
@@ -11,58 +11,58 @@ connection.onerror = function (error) {
   console.error('WebSocket Error ' + error);
 };
 
-var maps; 
-      var position={lat:19.024647,lng:99.943809};
-      function initMap(){
-        maps = new google.maps.Map(document.getElementById('map'),{
-              center:position,
-              zoom: 15,
-        });
-        var geocoder = new google.maps.Geocoder();
-        document.getElementById('submit').addEventListener('click',function(){
-          geocoderAddress(geocoder,maps);
-        });
-      }
-      function geocoderAddress(geocoder,resultsMap){
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address':address},function(results,status){
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map:resultsMap,
-              position:results[0].geometry.location,
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: '+status);
-          }
-        });
-      }
-      $(document).ready(function(){
-        $('.request').click(function () {
-          $('.popup_box').css({
-            "opacity": "1", "pointer-events": "auto"
-          });
-        });
-        $('.btn1').click(function () {
-          $('.popup_box').css({
-            "opacity": "0", "pointer-events": "none"
-          });
-        });
-        $('.btn2').click(function () {
-          $('.popup_box').css({
-            "opacity": "0", "pointer-events": "none"
-          });
-        });
-          $("#Logout").click(function (e) { 
-          e.preventDefault();
-          window.location.replace('/auth/logout')
+var maps;
+var position = { lat: 19.024647, lng: 99.943809 };
+function initMap() {
+  maps = new google.maps.Map(document.getElementById('map'), {
+    center: position,
+    zoom: 15,
+  });
+  var geocoder = new google.maps.Geocoder();
+  document.getElementById('submit').addEventListener('click', function () {
+    geocoderAddress(geocoder, maps);
+  });
+}
+function geocoderAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location,
       });
-        $("#profile").click(function (e) { 
-          e.preventDefault();
-          window.location.replace('/profile')
-      });
-      })
-      // This example creates a 2-pixel-wide red polyline showing the path of
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+$(document).ready(function () {
+  $('.request').click(function () {
+    $('.popup_box').css({
+      "opacity": "1", "pointer-events": "auto"
+    });
+  });
+  $('.btn1').click(function () {
+    $('.popup_box').css({
+      "opacity": "0", "pointer-events": "none"
+    });
+  });
+  $('.btn2').click(function () {
+    $('.popup_box').css({
+      "opacity": "0", "pointer-events": "none"
+    });
+  });
+  $("#Logout").click(function (e) {
+    e.preventDefault();
+    window.location.replace('/auth/logout')
+  });
+  $("#profile").click(function (e) {
+    e.preventDefault();
+    window.location.replace('/profile')
+  });
+})
+// This example creates a 2-pixel-wide red polyline showing the path of
 // the first trans-Pacific flight between Oakland, CA, and Brisbane,
 // Australia which was made by Charles Kingsford Smith.
 function initMap() {
@@ -76,28 +76,28 @@ function initMap() {
   directionsRenderer.setMap(map);
   calculateAndDisplayRoute(directionsService, directionsRenderer);
 
- 
+
   var image = '/image/car_13260.png';
-  var myLatLng = new google.maps.LatLng(19.024647,99.943809); //or wherever you want the marker placed
+  var myLatLng = new google.maps.LatLng(19.024647, 99.943809); //or wherever you want the marker placed
   beachMarker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: image
-      
+    position: myLatLng,
+    map: map,
+    icon: image
+
   });
-  
- 
+
+
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
   // var driver = '"'+lat+','+lng+'"'
   directionsService
     .route({
-      origin: 
+      origin:
         "19.024647,99.943809"
       ,
-      destination: 
-       "19.040660, 99.931254"
+      destination:
+        "19.040660, 99.931254"
       ,
       travelMode: google.maps.TravelMode.DRIVING,
     })
@@ -109,23 +109,23 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
 
 connection.onmessage = function (e) {
   var yourString = e.data;
-var array = [];
-yourString.split(':').forEach(function(value) {
-  array.push(value.split(' '));
-});
+  var array = [];
+  yourString.split(':').forEach(function (value) {
+    array.push(value.split(' '));
+  });
   // log ค่าที่ถูกส่งมาจาก server
   // console.log("message from server: ", e.data);
   // const map = new google.maps.Map(document.getElementById("map"), {
   //   zoom: 18,
   //   center: { lat: 19.024647, lng: 99.943809 },
   // });
-  
-  lat=parseFloat(array[2])    
-  lng=parseFloat(array[4])
+
+  lat = parseFloat(array[2])
+  lng = parseFloat(array[4])
   var latlng = new google.maps.LatLng(lat, lng);
   redraw()
   beachMarker.setPosition(latlng);
-  
+
 };
 
 // var numDeltas = 100;
