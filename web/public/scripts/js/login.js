0c49c01aedd36fba8db6e39cd625bea1ec4246b4
hondahwn <79396857+hondahwn@users.noreply.github.com>
2021-10-26 00:38:42 +0700

    ทำหน้าล็อคอิน

--- web/public/scripts/js/login.js
+++ web/public/scripts/js/login.js
@@ -8,5 +8,17 @@ $(document).ready(function () {
         window.location.href = "/auth/google";
         
     });
+
+//     const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
     
-})
+ })
