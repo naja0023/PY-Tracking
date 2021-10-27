98bb943eeb940d652c5e02f2609a2157d998516e
suphakorn <naja.67130@gmail.com>
2021-10-27 19:28:14 +0700

    bcryp password

--- web/routes/manageuserroute.js
+++ web/routes/manageuserroute.js
@@ -3,6 +3,7 @@ const config = require("../config/dbConfig.js");
 const mysql = require("mysql");
 const con = mysql.createConnection(config);
 const checkUser = require('./middleware');
+const bcrypt = require("bcryptjs");
 
 
 router.get("/newadminse", checkUser, (req, res) => {
@@ -27,11 +28,11 @@ router.get("/newadminse", checkUser, (req, res) => {
                             res.status(503).send("Database error");
                             return;
                         } else {
-                           
-                            res.render('newadmin', { count: result2 ,resule: result,count1: result3 })           
+
+                            res.render('newadmin', { count: result2, resule: result, count1: result3 })
                         }
                     })
-                    
+
                 }
             })
         }
@@ -52,7 +53,42 @@ router.get("/countdriver", checkUser, (req, res) => {
 })
 
 //  --- add new post ---
-router.post('/adminse/new', checkUser, (req, res) => {
+// router.post('/adminse/new', checkUser, (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     const name = req.body.name;
+//     const lastname = req.body.lastname;
+//     const email = req.body.email;
+//     const tell = req.body.tell;
+//     const id_card = req.body.id_card;
+//     const address = req.body.address;
+//     const sub = req.body.sub;
+//     const dist = req.body.dist;
+//     const prov = req.body.prov;
+//     const zip = req.body.zip;
+//     const sex = req.body.sex;
+//     const role = req.body.role;
+
+//     const sql = "INSERT INTO driver(username,password,name,lastname,email,tell,id_card,address,sub,dist,prov,zip,sex,role)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+//     con.query(sql, [username, password, name, lastname, email, tell, id_card, address, sub, dist, prov, zip, sex, role], function(err, result, fields) {
+//         if (err) {
+//             console.error(err.message);
+//             res.status(503).send("Database error");
+//             return;
+//         }
+
+//         // get inserted rows
+//         const numrows = result.affectedRows;
+//         if (numrows != 1) {
+//             console.error("can not insert data");
+//             res.status(503).send("Database error");
+//         } else {
+//             res.send("/newadminse");
+//         }
+//     });
+// });
+
+router.post("/adminse/new", function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
     const name = req.body.name;
@@ -68,21 +104,38 @@ router.post('/adminse/new', checkUser, (req, res) => {
     const sex = req.body.sex;
     const role = req.body.role;
 
-    const sql = "INSERT INTO driver(username,password,name,lastname,email,tell,id_card,address,sub,dist,prov,zip,sex,role)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
-    con.query(sql, [username, password, name, lastname, email, tell, id_card,address,sub,dist,prov,zip,sex,role], function(err, result, fields) {
+    //checked existing username
+    let sql = "SELECT driver_id FROM driver WHERE username=?";
+    con.query(sql, [username], function(err, result, fields) {
         if (err) {
             console.error(err.message);
-            res.status(503).send("Database error");
+            res.status(500).send("Database server error");
             return;
         }
 
-        // get inserted rows
-        const numrows = result.affectedRows;
-        if (numrows != 1) {
-            console.error("can not insert data");
-            res.status(503).send("Database error");
+        const numrows = result.length;
+        //if repeated username
+        if (numrows > 0) {
+            res.status(400).send("ขออภัย, ไอดีผู้ใช้นี้ถูกใช้งานแล้ว");
         } else {
-            res.send("/newadminse");
+            bcrypt.hash(password, 10, function(err, hash) {
+                //return hashed password, 60 characters
+                sql = "INSERT INTO driver(username,password,name,lastname,email,tell,id_card,address,sub,dist,prov,zip,sex,role)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                con.query(sql, [username, hash, name, lastname, email, tell, id_card, address, sub, dist, prov, zip, sex, role], function(err, result, fields) {
+                    if (err) {
+                        console.error(err.message);
+                        res.status(500).send("Database server error");
+                        return;
+                    }
+
+                    const numrows = result.affectedRows;
+                    if (numrows != 1) {
+                        res.status(500).send("Insert failed");
+                    } else {
+                        res.status(200).send('/newadminse')
+                    }
+                });
+            });
         }
     });
 });
@@ -105,14 +158,32 @@ router.put('/adminse/edit', checkUser, (req, res) => {
     const role = req.body.role;
     const driver_id = req.body.driver_id;
     console.log(req.body)
-    const sql = "UPDATE driver SET username=?,password=?,name=?,lastname=?,email=?,tell=?,id_card=?,address=?,sub=?,dist=?,prov=?,zip=?,sex=?,role=? WHERE driver_id=?"
-    con.query(sql, [username, password, name, lastname, email,tell, id_card,address,sub,dist,prov,zip,sex, role, driver_id], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(503).send("Server error");
-        } else {
-            res.status(200).send('/newadminse');
-        }
+        // const sql = "UPDATE driver SET username=?,password=?,name=?,lastname=?,email=?,tell=?,id_card=?,address=?,sub=?,dist=?,prov=?,zip=?,sex=?,role=? WHERE driver_id=?"
+        // con.query(sql, [username, password, name, lastname, email, tell, id_card, address, sub, dist, prov, zip, sex, role, driver_id], (err, result) => {
+        //     if (err) {
+        //         console.log(err);
+        //         res.status(503).send("Server error");
+        //     } else {
+        //         res.status(200).send('/newadminse');
+        //     }
+        // });
+    bcrypt.hash(password, 10, function(err, hash) {
+        //return hashed password, 60 characters
+        sql = "UPDATE driver SET username=?,password=?,name=?,lastname=?,email=?,tell=?,id_card=?,address=?,sub=?,dist=?,prov=?,zip=?,sex=?,role=? WHERE driver_id=?";
+        con.query(sql, [username, hash, name, lastname, email, tell, id_card, address, sub, dist, prov, zip, sex, role, driver_id], function(err, result, fields) {
+            if (err) {
+                console.error(err.message);
+                res.status(500).send("Database server error");
+                return;
+            }
+
+            const numrows = result.affectedRows;
+            if (numrows != 1) {
+                res.status(500).send("Insert failed");
+            } else {
+                res.status(200).send('/newadminse')
+            }
+        });
     });
 });
 
